1323f531888ab88dc9ead948b11f7d9f53e2c022
Aryan Kumar <aryankumar79922@gmail.com>
2024-08-13 20:09:40 +0530

    L3.2
    1. Entity & Table annotation working.
    2. Common hibernate config.
    3. Connecting our DB with localhost SQL DB.
    4. What is Hibernate.
    5. How JPA work with hibernate.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.product_table (price, quantity, sku, title_x)
+VALUES
+(12.4, 4, 'parle734', 'Parle Biscuit'),
+(14.4, 1, 'pepsi123', 'Pepsi'),
+(34.4, 10, 'Coke124', 'Coca Cola'),
+(16.4, 3, 'pepsi156', 'Mazza');
